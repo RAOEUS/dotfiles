aa2482cb0d4a4c141b9d332edb51d100efe83af7
Zach Rice <bynxmusic@gmail.com>
2023-12-20 23:40:21 -0500

    Added laptop trackpad settings for dwm

--- dwm-startup.sh
+++ dwm-startup.sh
@@ -2,8 +2,7 @@
 
 # Display Configurations
 # Due to .75x.75 scaling, the position of display 2 is moved over 1920 instead of 2560
-xrandr --output DisplayPort-1 --mode 2560x1440 --scale 1x1 --pos 0x0 --rotate normal --rate 143.86
-xrandr --output DisplayPort-2 --mode 2560x1440 --scale 1x1 --pos 2560x0 --rotate normal --rate 143.86
+xrandr --output eDP-1 --mode 1920x1080 --scale 1x1 --pos 0x0 --rotate normal --rate 144.00
 
 # Kensington Slimblade Wireless Config
 xinput --set-prop 16 'libinput Accel Speed' 0.2
@@ -11,6 +10,11 @@ xinput --set-prop 16 "libinput Middle Emulation Enabled" 1
 # Kensington Slimblade Wired Config
 xinput --set-prop 18 'libinput Accel Speed' 0.2
 xinput --set-prop 18 "libinput Middle Emulation Enabled" 1
+# Laptop trackpad settings
+xinput set-prop 14 "libinput Tapping Enabled" 1
+xinput set-prop 14 "libinput Tapping Button Mapping Enabled" 1, 0
+xinput set-prop 14 "libinput Natural Scrolling Enabled" 1
+
 
 # Background image
 feh --bg-fill /home/zach/Pictures/wallpaper-xianxia-dragon.jpg
