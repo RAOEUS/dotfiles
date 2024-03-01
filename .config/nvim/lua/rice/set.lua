ac90b88ea3696c8173ccff12ad39e3379304f506
Zach Rice <bynxmusic@gmail.com>
2024-03-01 11:28:45 -0500

    added suda.vim plugin

--- .config/nvim/lua/rice/set.lua
+++ .config/nvim/lua/rice/set.lua
@@ -1,4 +1,4 @@
-vim.opt.guicursor = ""
+vim.opt.compatible = false
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -17,7 +17,6 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
